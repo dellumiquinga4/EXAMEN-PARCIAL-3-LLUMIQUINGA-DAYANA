f32ef6a5b8a7a3d1e6adae2f4a047ea5d53225a4
dellumiquinga4 <dellumiquinga4@espe.edu.ec>
2025-03-12 20:58:15 -0500

    3
    3 cambios

--- sistema/src/main/java/com/facturacion/sistema/client/ProductoClient.java
+++ sistema/src/main/java/com/facturacion/sistema/client/ProductoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "producto-service", url = "${app.producto-service.url}")
+@FeignClient(name = "producto-service", url = "http://localhost:8081")
 public interface ProductoClient {
     
     @GetMapping("/v1/productos/{codProducto}")
